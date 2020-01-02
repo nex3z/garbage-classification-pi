40f4dc7c2a8655903fefabd3a9ababd2912aa90b
nex3z <litianxing9@gmail.com>
2020-01-02 22:57:50 +0800

    Update utils.

--- utils.py
+++ utils.py
@@ -14,3 +14,15 @@ def preprocess(image, target_size):
     image /= 127.5
     image -= 1.
     return image
+
+
+def center_crop(image):
+    width, height = image.size
+    if width / height < 1:
+        vertical_offset = int((height - width) / 2)
+        padded = image.crop((0, vertical_offset, width, height - vertical_offset))
+    else:
+        horizontal_offset = int((width - height) / 2)
+        padded = image.crop((horizontal_offset, 0, width - horizontal_offset, height))
+    return padded
+    
